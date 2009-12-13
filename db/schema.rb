599230339d5cad3459c8c076b0b6b028d745a190
Andrew Carpenter <acarpen@wested.org>
2009-12-13 15:13:46 -0800

    add active flag to users

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091210161339) do
+ActiveRecord::Schema.define(:version => 20091213224300) do
 
   create_table "cell_sites", :force => true do |t|
     t.float    "lat"
@@ -42,14 +42,14 @@ ActiveRecord::Schema.define(:version => 20091210161339) do
 
   create_table "users", :force => true do |t|
     t.string   "login"
-    t.string   "email",                              :null => false
-    t.string   "crypted_password",                   :null => false
-    t.string   "password_salt",                      :null => false
-    t.string   "persistence_token",                  :null => false
-    t.string   "single_access_token",                :null => false
-    t.string   "perishable_token",                   :null => false
-    t.integer  "login_count",         :default => 0, :null => false
-    t.integer  "failed_login_count",  :default => 0, :null => false
+    t.string   "email",                                  :null => false
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistence_token",                      :null => false
+    t.string   "single_access_token",                    :null => false
+    t.string   "perishable_token",                       :null => false
+    t.integer  "login_count",         :default => 0,     :null => false
+    t.integer  "failed_login_count",  :default => 0,     :null => false
     t.datetime "last_request_at"
     t.datetime "current_login_at"
     t.datetime "last_login_at"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(:version => 20091210161339) do
     t.string   "last_login_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "active",              :default => false, :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
