8ef7869b651a0a9cd7d37edc3f704e9605226ad6
Bob Burbach <rburbach@bob-burbachs-macbook.local>
2009-12-19 12:22:40 -0800

    Fix register and login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
   end
   
   def create
-    @user_session = UserSession.new(params[:user_session])
+    @user_session = UserSession.new(params[:user].except(:sign_in_type))
     if @user_session.save
       flash[:notice] = "Login successful!"
       redirect_to account_url
