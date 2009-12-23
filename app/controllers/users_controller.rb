61eb4aad223f117b5cf028228e07e0ab9dfa9125
Andrew Carpenter <acarpen@wested.org>
2009-12-23 13:25:13 -0800

    after user update, redirect back to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     @user = @current_user # makes our views "cleaner" and more consistent
     if @user.update_attributes(params[:user])
       flash[:notice] = "Account updated!"
-      redirect_to root_url
+      redirect_to account_url
     else
       render :action => :edit
     end
