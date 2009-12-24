8b506037fa311cd82535cfb66766260ddf1cff5f
Andrew Carpenter <acarpen@wested.org>
2009-12-24 09:58:43 -0800

    only most recent two cell sites

--- app/controllers/special_controller.rb
+++ app/controllers/special_controller.rb
@@ -1,7 +1,7 @@
 class SpecialController < ApplicationController
   def home
     @map = create_map(current_location.lat, current_location.lng)
-    @recently_added = CellSite.from_active_user.all(:limit => 5, :order => "cell_sites.created_at DESC")
+    @recently_added = CellSite.from_active_user.all(:limit => 2, :order => "cell_sites.created_at DESC")
   end
   
   def api
