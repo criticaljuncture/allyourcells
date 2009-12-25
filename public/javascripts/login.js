e48d20377a788d5c1ad775d018ac5cf147377b0f
Bob Burbach <rburbach@bob-burbachs-macbook.local>
2009-12-24 22:12:15 -0800

    Add the >> back into login buttons that are modified with js

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
   $('#user_sign_in_type_login').bind('change', function(e){
     e.preventDefault();
     login_target_form.attr('action', '/user_session');
-    $('.signin_form input#user_submit').val('Sign In');
+    $('.signin_form input#user_submit').val('Sign In &raquo;');
   });
   
   $('#user_sign_in_type_register').bind('change', function(e){
     e.preventDefault();
     login_target_form.attr('action', '/users');
-    $('.signin_form input#user_submit').val('Sign Up');
+    $('.signin_form input#user_submit').val('Sign Up &raquo;');
   });
 
 });
