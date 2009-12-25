80332e06d843034527ee07561f8cc2f811c91a15
Andrew Carpenter <acarpen@wested.org>
2009-12-25 14:56:16 -0800

    add hoptoad API key configuration

--- config/initializers/api_keys.rb
+++ config/initializers/api_keys.rb
@@ -2,4 +2,5 @@ config = YAML::load(File.open("#{RAILS_ROOT}/config/api_keys.yml"))
 
 config.each_pair do |key, value|
   ENV["#{key}_api_key"] = value
+  self.class.const_set("#{key.upcase}_API_KEY", value)
 end
---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = HOPTOAD_API_KEY
+end
