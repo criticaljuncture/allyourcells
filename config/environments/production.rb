976662ad413cb5a5d4ec5e2abda68fdce1923097
Bob Burbach <rburbach@bob-burbachs-macbook.local>
2009-12-31 20:24:39 -0800

    Uncomment production file as this is needed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ config.action_mailer.default_url_options = {:host => "allyourcells.com"}
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
     if forked
-      #config.logger = MongoLogger.new(2) # 2 = WARN see activesupport-2.3.5/lib/active_support/buffered_logger.rb for severity constant values
+      config.logger = MongoLogger.new(2) # 2 = WARN see activesupport-2.3.5/lib/active_support/buffered_logger.rb for severity constant values
     end
   end
 end
