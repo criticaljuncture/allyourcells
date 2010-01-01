27b7d8a1e3410cfb17ab3b882c02f6253b04f652
Bob Burbach <rburbach@bob-burbachs-macbook.local>
2009-12-31 19:11:55 -0800

    Add code to handle passenger forking of process. Now creates new db connection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,4 +29,10 @@ config.action_mailer.default_url_options = {:host => "allyourcells.com"}
 # config.threadsafe!
 
 # use mongo as rails log
-config.logger = MongoLogger.new(2) # 2 = WARN see activesupport-2.3.5/lib/active_support/buffered_logger.rb for severity constant values
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    if forked
+      #config.logger = MongoLogger.new(2) # 2 = WARN see activesupport-2.3.5/lib/active_support/buffered_logger.rb for severity constant values
+    end
+  end
+end
