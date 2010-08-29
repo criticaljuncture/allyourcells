cd7e767362da7f1dd79c4311e59545f5d4ceca34
Bob Burbach <bob.burbach@gmail.com>
2010-08-28 18:17:22 -0700

    Update deploy file for ec2 and git-based deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,64 @@
+# this is needed for running capistrano locally on a server
+# without it all our gems aren't avail to be loaded below
+require File.expand_path(File.join(File.dirname(__FILE__), "..", ".bundle", "environment"))
+
+# hoptoad deploy notifications, etc
+#require 'hoptoad_notifier/capistrano'
+
+# deploy recipes - need to do `sudo gem install thunder_punch` - these should be required last
+require 'thunder_punch'
+
+
 #############################################################
 # Set Basics
 #############################################################
 
 set :application, "allyourcells"
 set :user, "deploy"
-set :port, 22
+
+# ssh key setup
+if File.exists?(File.join(ENV["HOME"], ".ssh", "govpulse-prod-provision"))
+  ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "govpulse-prod-provision")]
+else
+  puts "~/.ssh/govpulse-prod-provision was not available falling back to ~/.ssh/id_rsa"
+  ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
+end
+
 
 ssh_options[:paranoid] = false
 set :use_sudo, true
+default_run_options[:pty] = true
 
-set :keep_releases, 15
+
+#############################################################
+# Git-based Deployment Setup
+#############################################################
+
+set(:latest_release)  { fetch(:current_path) }
+set(:release_path)    { fetch(:current_path) }
+set(:current_release) { fetch(:current_path) }
+
+set(:current_revision)  { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:latest_revision)   { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:previous_revision) { capture("cd #{current_path}; git rev-parse --short HEAD@{1}").strip }
+
+
+set :finalize_deploy, false
+# we don't need this because we have an asset server
+# and we also use varnish as a cache server. Thus 
+# normalizing timestamps is detrimental.
+set :normalize_asset_timestamps, false
+
+
+set :migrate_target, :current
+
 
 #############################################################
 # Set Branch
 #############################################################
 
-set(:branch) do
-  current_branch   = `git branch`.match(/\* (.*)/)[1]
-  specified_branch = Capistrano::CLI.ui.ask "Branch [#{current_branch}]: "
-  specified_branch == '' ? current_branch : specified_branch
-end
+set(:branch) { `git branch`.match(/\* (.*)/)[1] }
+
 
 #############################################################
 # General Settings  
@@ -27,40 +66,35 @@ end
 
 set :rails_env,  "production"                           
 set :deploy_to,  "/var/www/apps/#{application}" 
-set :domain,     "204.232.210.89"                
+set :domain,     "ec2-184-73-55-26.compute-1.amazonaws.com" #ec2 large server             
 set :url,        "#{domain}"     
 set :server_url, "#{domain}"
 
-role :web, domain                      
-role :app, domain                      
-role :db , domain, {:primary => true}
+role :proxy,  domain
+role :static, domain
+role :worker, domain, {:primary=>true}
+role :app,    domain
+role :db ,    domain, {:primary => true}
+
 
 #############################################################
 # Database Settings
 #############################################################
 
 set :remote_db_name, "allyourcells_production"
-set :db_dump_name,   "#{remote_db_name}_#{Time.now.strftime('%Y%m%d')}.sql"
 set :db_path,        "#{shared_path}/db"
-set :sql_file_path,  "#{db_path}/#{db_dump_name}"
+set :sql_file_path,  "#{shared_path}/db/#{remote_db_name}_#{Time.now.utc.strftime("%Y%m%d%H%M%S")}.sql"
 
 
 #############################################################
 # SCM Settings
 #############################################################
 set :scm,              :git          
-set :github_user_repo, 'trifecta'
+set :github_user_repo, 'criticaljuncture'
 set :github_project_repo, 'allyourcells'
 set :deploy_via,       :remote_cache 
-
-
-#############################################################
-# If for some reason the server can not resolve github, use
-# set :deploy_via, :copy
-# 
-# This will deploy from your local copy of the repository -
-# it may take a while...
-#############################################################
+set :repository, "git@github.com:#{github_user_repo}/#{github_project_repo}.git"
+set :github_username, 'criticaljuncture'
 
 
 #############################################################
@@ -73,33 +107,29 @@ set :git_enable_submodules, true
 
 
 #############################################################
-# Github
-#############################################################
-
-if deploy_via == :remote_cache
-
-  set :deploy_via, :remote_cache
-  set :repository, "git@github.com:#{github_user_repo}/#{github_project_repo}.git"
-  set :github_username, 'trifecta'
-
-elsif deploy_via == :copy
-
-  set :copy_cache, true
-  set :repository, '.git'
-
-end
-
-#############################################################
 # Run Order
 #############################################################
 
 # Do not change below unless you know what you are doing!
-after "deploy:update_code",       "deploy:update_config"
-after "deploy",                   "deploy:cleanup"
-after "deploy:cleanup",           "deploy:set_rake_path"
-after "deploy:set_rake_path",     "deploy:install_gems"
-after "deploy:install_gems",      "deploy:migrate"
+after "deploy:update_code",       "symlinks:create"
+after "symlinks:create",          "deploy:set_rake_path"
+after "deploy:set_rake_path",     "bundler:fix_bundle"
+after "bundler:fix_bundle",       "deploy:migrate"
 after "deploy:migrate",           "passenger:restart"
+
+
+#############################################################
+# Symlinks for Static Files
+#############################################################
+set :custom_symlinks, {
+  'config/api_keys.yml'                       => 'config/api_keys.yml',
+  'config/mail.yml'                           => 'config/mail.yml',
+  'config/initializers/cloudkicker_config.rb' => 'config/cloudkicker_config.rb',
+  
+  'data' => 'data',
+}
+
+
 
 
 #############################################################
@@ -118,175 +148,13 @@ after "deploy:migrate",           "passenger:restart"
 namespace :deploy do
   desc "Creates project directories with appropriate permissions"
   task :create_project_directories do
-    sudo "mkdir -p #{deploy_to}"
     sudo "mkdir -p #{deploy_to}/releases"
     sudo "mkdir -p #{deploy_to}/shared/log"
     sudo "mkdir -p #{deploy_to}/shared/data"
+    sudo "mkdir -p #{deploy_to}/shared/system"
     sudo "chown -R #{user}:#{user} #{deploy_to}"
     sudo "touch #{deploy_to}/shared/log/#{rails_env}.log"
     sudo "chmod 0666 #{deploy_to}/shared/log/#{rails_env}.log"
   end
 end
 
-
-#############################################################
-# Set Rake Path
-#############################################################
-
-namespace :deploy do
-  desc "Set rake path"
-  task :set_rake_path, :roles => [:app] do
-    run "which rake" do |ch, stream, data|
-      if stream == :err
-        abort "captured output on STDERR when setting rake path: #{data}"
-      elsif stream == :out
-        set :rake_path, data.to_s.strip
-      end
-    end
-  end
-end
-
-
-#############################################################
-# Symlinks for Static Files
-#############################################################
-
-namespace :deploy do
-  desc "Set Symlinks for Static Files (like database.yml)"
-  task :update_config, :roles => [:app] do
-    %w(database.yml api_keys.yml mail.yml).each do |file|
-      sudo "ln -sf #{shared_path}/config/#{file} #{release_path}/config/#{file}"
-    end
-    sudo "ln -sf #{shared_path}/config/cloudkicker_config.rb #{release_path}/config/initializers/cloudkicker_config.rb"
-    sudo "ln -sf #{shared_path}/log #{release_path}/log"
-    sudo "ln -sf #{shared_path}/data #{release_path}/data"
-  end 
-end
-
-
-#############################################################
-# Install Missing Gems
-#############################################################
-
-namespace :deploy do
-  desc "Install Missing Gems"
-  task :install_gems, :roles => [:app] do
-    run "cd #{release_path}; sudo #{rake_path} RAILS_ENV=#{rails_env} gems:install --trace"
-  end
-end
-
-
-#############################################################
-# Load Default Data
-#############################################################
-
-namespace :deploy do
-  desc "Load Default Data"
-  task :load_default_data, :roles => [:app] do
-    #run "cd #{release_path}; sudo #{rake_path} RAILS_ENV=#{rails_env} data:load:#{rails_env}:all"
-  end
-end
-
-#############################################################
-# Passenger Restart
-#############################################################
-
-namespace :deploy do
-  desc "Remove deploy:restart In Favor Of passenger:restart Task"
-  task :restart do
-  end
-end
-
-namespace :passenger do
-  desc "Restart Application"
-  task :restart do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
-end
-
-#############################################################
-# Passenger Status Checks
-#############################################################
-
-namespace :passenger do
-  desc "Check Passenger Status"
-  task :status do
-    sudo 'passenger-status'
-  end
-
-  desc "Check Apache/Passenger Memory Usage"
-  task :memory_stats do
-    sudo 'passenger-memory-stats'
-  end
-end
-
-#############################################################
-# Check for Existing Database
-#############################################################
-
-namespace :database do
-  desc "Check to see if database exists"
-  task :check_database_existence do
-    db_exists = false
-    run "mysql -u root -e \"SELECT COUNT(SCHEMA_NAME) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '#{database_to_check}';\" --batch --reconnect --show-warning --silent" do |ch, stream, data|
-      if stream == :err
-        abort "capured output on STDERR when verifying database #{database_to_check} exists: #{data}"
-      elsif stream == :out
-        return_value = data.split("\n").first.chomp
-    
-        if ['0','1'].include?(return_value)
-          db_exists = return_value == '0' ? false : true
-        else
-          abort "Invalid response from db when checking if database exists. Expected 0 or 1, got a count of #{return_value}"
-        end
-        puts db_exists
-      end
-    end
-    db_exists
-  end
-end
-
-#############################################################
-# Backup Database
-#############################################################
-
-namespace :database do
-  desc "Dump the current database"
-  task :backup do
-    set :database_to_check, remote_db_name # used by the check_database_existence task
-    db_exists = find_and_execute_task('database:check_database_existence')
-  
-    if db_exists 
-      sudo "sudo mkdir -p #{db_path}"
-      run "mysqldump -u root --opt #{remote_db_name} > #{sql_file_path}"
-    else
-      abort "There is no database named #{remote_db_name} to backup"
-    end
-  end #end task :backup
-end #end namspace 
-
-#############################################################
-# Load Remote Staging Database to Local Machine
-#############################################################
-
-namespace :database do
-
-  desc "Backup remote database and load locally"
-  task :load_remote do
-    backup
-    copy
-    load_copy
-  end
-
-  desc "Copy the current database" 
-  task :copy do
-    `mkdir -p tmp`
-    download(sql_file_path, "tmp/", :via=> :scp)
-  end
-
-  desc "Load the staging database locally"
-  task :load_copy do
-    `script/dbconsole -p < tmp/#{db_dump_name}`
-  end
-
-end #end namspace
